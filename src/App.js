998713917c57fa1b629aa8aa7c2681173511118e
whatthefoobar <irina.mntn@gmail.com>
2022-05-30 10:27:50 +0200

    loaded content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="accordion">
+        {data.map((item, i) => (
+          <div className="item" key={i}>
+            <div className="title">
+              <h2>{item.question}</h2>
+            </div>
+            <div className="content">{item.answer}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
+const data = [
+  {
+    question: 'How many team members can I invite?',
+    answer:
+      'You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.',
+  },
+  {
+    question: 'What is the maximum file upload size?',
+    answer:
+      'No more than 2GB. All files in your account must fit your allotted storage space.',
+  },
+  {
+    question: 'How do I reset my password?',
+    answer:
+      'Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.',
+  },
+  {
+    question: 'Can I cancel my subscription?',
+    answer:
+      "Yes! Send us a message and we'll process your request no questions asked.",
+  },
+  {
+    question: 'Do you provide additional support?',
+    answer:
+      'Chat and email support is available 24/7. Phone lines are open during normal business hours.',
+  },
+];
 
 export default App;
